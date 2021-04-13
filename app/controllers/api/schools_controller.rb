1ad72a5b14b4633d69e15e7f600aab7c6eff901a
Yitzy Bratt <yitzybratt32@gmail.com>
2021-04-12 21:37:03 -0400

    show action

--- app/controllers/api/schools_controller.rb
+++ app/controllers/api/schools_controller.rb
@@ -1,7 +1,14 @@
 class Api::SchoolsController < ApplicationController
   require "http"
+
   def index
-    @request = HTTP.get("https://data.cityofnewyork.us/resource/f9bf-2cp4.json/")
-    render 'index.json.jb'
+    @request = JSON.parse(HTTP.get("https://data.cityofnewyork.us/resource/f9bf-2cp4.json/"))
+
+    render "index.json.jb"
+  end
+
+  def show
+    @school = JSON.parse(HTTP.get("https://data.cityofnewyork.us/resource/f9bf-2cp4.json?dbn=#{params[:dbn]}"))
+    render "show.json.jb"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
     get "/schools" => "schools#index"
+    get "/schools/:id" => "schools#show"
   end
 end
